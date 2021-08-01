e722d2239efcc3b295e7833f6ca304104c379e7a
IqraZareef-1024 <izareef2410@gmail.com>
2021-08-01 16:44:43 +0530

    uploading on github

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://anjali:aszxas@mycluster-kjvid.mongodb.net/test?retryWrites=true&w=majority'
+//dbPassword = 'mongodb+srv://iqra:iqra@cluster0.6zs7w.mongodb.net/Cluster0?retryWrites=true&w=majority'
+module.exports = {
+    mongoURI: dbPassword
+};
